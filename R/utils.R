8ab10f5edc92a49700c8e3054e749fcc6a0481d5
Bertrand Wilden <bert@bwilden.com>
2020-12-07 13:52:10 -0800

    Initial commit

---
+++ R/utils.R
@@ -0,0 +1,7 @@
+
+arg_max_cols <- function(df, num_cols, max_col_name = "max_col") {
+  cols <- df[utils::tail(seq_along(df), num_cols)]
+  max_col <- colnames(cols)[max.col(cols)]
+  df[[max_col_name]] <- max_col
+  return(df)
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(bper)
+
+test_check("bper")
