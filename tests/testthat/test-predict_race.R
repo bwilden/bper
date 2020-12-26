21dd669d83c32e4ca421db6f68607e5a332ab1a7
Bertrand Wilden <bert@bwilden.com>
2020-12-25 19:43:53 -0600

    initial commit

---
+++ tests/testthat/test-predict_race.R
@@ -0,0 +1,12 @@
+
+
+
+test_people <- data.frame(first_name = c("BERTRAND"),
+                          last_name = c("WILDEN"),
+                          birth_year = c(1992),
+                          female = c(0),
+                          party = c("DEM"),
+                          apartment = c(1),
+                          zip = c("92092"),
+                          block = c(NA_character_),
+                          id = c(1))
