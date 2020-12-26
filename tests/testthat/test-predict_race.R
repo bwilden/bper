7688cda9d2322c50b44479c85f5ede524df863e0
Bertrand Wilden <bert@bwilden.com>
2020-12-25 20:56:40 -0600

    feat: test that predict_race returns a data.frame and that arg_max_cols picks correctly

--- tests/testthat/test-predict_race.R
+++ tests/testthat/test-predict_race.R
@@ -1,12 +1,25 @@
 
 
 
-test_people <- data.frame(first_name = c("BERTRAND"),
-                          last_name = c("WILDEN"),
-                          birth_year = c(1992),
-                          female = c(0),
-                          party = c("DEM"),
-                          apartment = c(1),
-                          zip = c("92092"),
-                          block = c(NA_character_),
-                          id = c(1))
+test_people <- data.frame(first_name = c("JAKE", "DESHAWN"),
+                          last_name = c("MUELLER", "JACKSON"),
+                          birth_year = c(1992, 1959),
+                          female = c(0, 0),
+                          party = c("DEM", "DEM"),
+                          apartment = c(1, 0),
+                          zip = c("92092", "70113"),
+                          block = c("010010201001000", NA_character_),
+                          id = c(1, 2))
+test_df <- predict_race(test_people)
+test_df
+
+test_that("returns a data.frame object", {
+  expect_is(test_df, "data.frame")
+})
+
+test_that("arg_max_cols chooses correct column", {
+  thin_df <- test_df[, -ncol(test_df)]
+  thin_df <- arg_max_cols(thin_df, 6, max_col_name = "pred_race")
+
+  expect_equal(thin_df$pred_race, paste0("prob_", test_df$pred_race))
+})
