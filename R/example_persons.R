00967f8c20367e41fce47a27218f0baa4667a2a7
Bertrand Wilden <bert@bwilden.com>
2020-12-27 17:16:06 -0600

    feat: added example data set for testing and vignettes

---
+++ R/example_persons.R
@@ -0,0 +1,13 @@
+
+
+example_persons <- data.frame(
+  first_name = c("BERTRAND", "LYNDON", "RAUL", "ANNA", "KARL", "MATHIEU", "LIAM"),
+  last_name = c("WILDEN", "WITHER", "VANDERWARKER", "ARENA", "SOM", "TURA", "SZYMONIAK"),
+  birth_year = c(1992, 1963, 1989, 1920, 1978, 1913, 1932),
+  female = c(0, 1, 1, 1, 0, 0, 1),
+  party = c("DEM", "DEM", "DEM", "REP", "UNA", "DEM", "REP"),
+  apartment = c(1, 0, 1, 1, 1, 1, 0),
+  zip = c("92092", "53146", "57551", "92844", "03862", "65557", "59730"),
+  block = c("060730083052007", "220419502002064", "420199112002033", "170318016082006", "480739503004014", "482239503001080", "450630210201000")
+)
+
