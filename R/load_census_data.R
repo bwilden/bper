3a2e7cccf5fc0e4dcc5ee79a486b431059fb6aed
Bertrand Wilden <bert@bwilden.com>
2021-06-14 13:25:00 -0500

    refactored sex age function

--- R/load_census_data.R
+++ R/load_census_data.R
@@ -45,112 +45,7 @@ load_surnames_data <- function(year) {
 }
 
 
-# Sex/Age -----------------------------------------------------------------
-
-get_sex_age_data <- function(census_group, group_name, year) {
-
-  labels <- censusapi::listCensusMetadata(
-    name = "dec/sf1",
-    vintage = year,
-    type = "variables",
-    group = census_group) %>%
-    filter(predicateType == "int") %>%
-    arrange(name)
-
-  sex_age_df <- censusapi::getCensus(
-    name = "dec/sf1",
-    vintage = year,
-    region = "us",
-    vars = paste0("group(", census_group, ")")
-  ) %>%
-    select(labels$name) %>%
-    `colnames<-`(labels$label) %>%
-    pivot_longer(cols = everything(),
-                 values_to = group_name,
-                 names_repair = "minimal")
-
-  return(sex_age_df)
-}
-
-load_sex_age_data <- function(year, vars) {
-
-  if (year == 2010) {
-    sex_age_totals <- get_sex_age_data("PCT12", "total", 2010) %>%
-      left_join(get_sex_age_data("PCT12H", "hispanic", 2010)) %>%
-      left_join(get_sex_age_data("PCT12I", "white", 2010)) %>%
-      left_join(get_sex_age_data("PCT12J", "black", 2010)) %>%
-      left_join(get_sex_age_data("PCT12K", "aian", 2010)) %>%
-      left_join(get_sex_age_data("PCT12L", "api1", 2010)) %>%
-      left_join(get_sex_age_data("PCT12M", "api2", 2010)) %>%
-      left_join(get_sex_age_data("PCT12N", "other1", 2010)) %>%
-      left_join(get_sex_age_data("PCT12O", "other2", 2010)) %>%
-      mutate(api = api1 + api2,
-             other = other1 + other2) %>%
-      select(-c(api1, api2, other1, other2))
-  }
-
-  if (vars == "both") {
-
-    sex_ages <- sex_age_totals %>%
-      filter(!name %in% c("Total", "Total!!Female", "Total!!Male")) %>%
-      mutate(
-        sex = if_else(grepl("Female", name), 1, 0),
-        age = if_else(grepl("Under", name), 0,
-                      as.numeric(str_extract(name, "[:digit:]+"))),
-        across(c(hispanic, white, black, aian, api, other),
-               ~ . / total,
-               .names = "pr_{.col}|sex_age"),
-        across(c(hispanic, white, black, aian, api, other),
-               ~ . / sum(.),
-               .names = "pr_sex_age|{.col}")
-      ) %>%
-      select(sex, age, contains("pr_"))
-
-    return(sex_ages)
-
-  } else if (vars == "sex") {
-
-    sexes <- sex_age_totals %>%
-      filter(name %in% c("Total!!Male", "Total!!Female")) %>%
-      mutate(
-        sex = if_else(grepl("Female", name), 1, 0),
-        across(c(hispanic, white, black, aian, api, other),
-               ~ . / total,
-               .names = "pr_{.col}|sex"),
-        across(c(hispanic, white, black, aian, api, other),
-               ~ . / sum(.),
-               .names = "pr_sex|{.col}")
-      ) %>%
-      select(sex, contains("pr_"))
-
-    return(sexes)
-
-  } else if (vars == "age") {
-
-    ages <- sex_age_totals %>%
-      filter(!name %in% c("Total", "Total!!Female", "Total!!Male")) %>%
-      mutate(age = if_else(grepl("Under", name), 0,
-                           as.numeric(str_extract(name, "[:digit:]+")))) %>%
-      group_by(age) %>%
-      summarise(across(-name, sum)) %>%
-      ungroup() %>%
-      mutate(
-        across(c(hispanic, white, black, aian, api, other),
-               ~ . / total,
-               .names = "pr_{.col}|age"),
-        across(c(hispanic, white, black, aian, api, other),
-               ~ . / sum(.),
-               .names = "pr_age|{.col}")
-      ) %>%
-      select(age, contains("pr_"))
-
-    return(ages)
-
-  }
-}
-
-
-# Multi-unit Occupancy ----------------------------------------------------
+# Multi-Unit Occupancy ----------------------------------------------------
 
 load_multi_unit_data <- function(year, census_groups) {
 
@@ -191,4 +86,95 @@ load_multi_unit_data <- function(year, census_groups) {
 
   return(multi_units)
 }
+
+
+# Sex/Age -----------------------------------------------------------------
+
+load_sex_age_data <- function(year, census_groups, vars) {
+
+  sex_age_totals <- tibble()
+  for (group in census_groups) {
+
+    sex_age_totals_group <- censusapi::getCensus(
+      name = "dec/sf1",
+      vintage = year,
+      region = "us",
+      vars = paste0("group(", group[1], ")")
+    ) %>%
+      select(contains("PCT")) %>%
+      rename_with(~ str_sub(., start = 8)) %>%
+      mutate(group := group[2])
+
+    sex_age_totals <- rbind(sex_age_totals, sex_age_totals_group)
+  }
+
+  labels <- censusapi::listCensusMetadata(
+    name = "dec/sf1",
+    vintage = year,
+    type = "variables",
+    group = census_groups[[1]][1]) %>%
+    mutate(name = str_sub(name, start = 8)) %>%
+    select(name, label)
+
+  ethnorace_columns <- unlist(census_groups)[c(F, T)]
+
+  sex_age_totals <- sex_age_totals %>%
+    pivot_longer(cols = !group) %>%
+    pivot_wider(names_from = group) %>%
+    left_join(labels, by = "name")
+
+  sex_ages <- sex_age_totals %>%
+    filter(!label %in% c("Total", "Total!!Female", "Total!!Male")) %>%
+    mutate(
+      sex = if_else(grepl("Female", label), 1, 0),
+      age = if_else(grepl("Under", label), 0,
+                    as.numeric(str_extract(label, "[:digit:]+"))),
+      across(ethnorace_columns,
+             ~ . / rowSums(across(ethnorace_columns)),
+             .names = "pr_{.col}|sex_age"),
+      across(ethnorace_columns,
+             ~ . / sum(.),
+             .names = "pr_sex_age|{.col}")
+    ) %>%
+    select(sex, age, contains("pr_"))
+
+  sexes <- sex_age_totals %>%
+    filter(label %in% c("Total!!Male", "Total!!Female")) %>%
+    mutate(
+      sex = if_else(grepl("Female", label), 1, 0),
+      across(ethnorace_columns,
+             ~ . / rowSums(across(ethnorace_columns)),
+             .names = "pr_{.col}|sex"),
+      across(ethnorace_columns,
+             ~ . / sum(.),
+             .names = "pr_sex|{.col}")
+    ) %>%
+    select(sex, contains("pr_"))
+
+  ages <- sex_age_totals %>%
+    filter(!label %in% c("Total", "Total!!Female", "Total!!Male")) %>%
+    mutate(age = if_else(grepl("Under", label), 0,
+                         as.numeric(str_extract(label, "[:digit:]+")))) %>%
+    group_by(age) %>%
+    summarise(across(-c(name, label), sum)) %>%
+    ungroup() %>%
+    mutate(
+      across(ethnorace_columns,
+             ~ . / rowSums(across(ethnorace_columns)),
+             .names = "pr_{.col}|age"),
+      across(ethnorace_columns,
+             ~ . / sum(.),
+             .names = "pr_age|{.col}")
+    ) %>%
+    select(age, contains("pr_"))
+
+  if (vars == "both") {
+    return(sex_ages)
+  } else if (vars == "sex") {
+    return(sexes)
+  } else if (vars == "age") {
+    return(ages)
+  }
+}
+
 
--- tests/testthat/test-load_census_data.R
+++ tests/testthat/test-load_census_data.R
@@ -1,12 +1,14 @@
 
-last_names <- load_surnames_data(year = 2010)
+# Last Names --------------------------------------------------------------
+
+last_names_test <- load_surnames_data(year = 2010)
 
 test_that("surnames data loads", {
-  expect_is(last_names, "data.frame")
+  expect_is(last_names_test, "data.frame")
 })
 
 test_that("surnames data has correct columns", {
-  expect_equal(colnames(last_names),
+  expect_equal(colnames(last_names_test),
                c("last_name", "pr_white|last",
                  "pr_black|last", "pr_aian|last",
                  "pr_api|last", "pr_hispanic|last",
@@ -16,14 +18,30 @@ test_that("surnames data has correct columns", {
                  "pr_last|other"))
 })
 
-sex_age_totals <- get_sex_age_data("PCT12", "total", 2010)
 
-test_that("sex_age_totals data loads", {
-  expect_is(sex_age_totals, "data.frame")
+# Sex/Age -----------------------------------------------------------------
+
+sex_ages_test <- load_sex_age_data(year = 2010, vars = "both",
+                                  list(
+                                    c("PCT12H", "hispanic"),
+                                    c("PCT12I", "white"),
+                                    c("PCT12J", "black")
+                                  ))
+
+test_that("sex_ages data loads", {
+  expect_is(sex_ages_test, "data.frame")
 })
 
-sex_age <- load_sex_age_data(year = 2010, vars = "both")
 
-test_that("sex_age_totals data loads", {
-  expect_is(sex_age, "data.frame")
+# Multi-Unit Occupancy ----------------------------------------------------
+
+multi_unit_test <- load_multi_unit_data(year = 2019,
+                                        census_groups = list(
+                                          c("B25032H", "white"),
+                                          c("B25032I", "hispanic"),
+                                          c("B25032B", "black")
+                                        ))
+
+test_that("multi_unit data loads", {
+  expect_is(multi_unit_test, "data.frame")
 })
