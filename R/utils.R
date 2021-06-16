ff0370060da9069f24b93ff15f6d13ae0d4cc032
Bertrand Wilden <bert@bwilden.com>
2021-06-16 18:32:56 -0500

    removed arg_max_cols

--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,3 @@
 
-arg_max_cols <- function(df, num_cols, max_col_name = "max_col") {
-  cols <- df[utils::tail(seq_along(df), num_cols)]
-  max_col <- colnames(cols)[max.col(cols)]
-  df[[max_col_name]] <- max_col
-
-  return(df)
-}
-
+# The opposite of %in%
 `%notin%` <- Negate(`%in%`)
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,40 +1,5 @@
 
 
-# arg_max_cols() ----------------------------------------------------------
-
-df <- data.frame(
-  a = c(0.3, 0.5),
-  b = c(0.2, 0.7),
-  c = c(0.1, 0.8)
-)
-
-test_that("returns appropriate data frame", {
-  arg_max_df <- arg_max_cols(df, num_cols = ncol(df))
-
-  expect_is(arg_max_df, "data.frame")
-  expect_is(arg_max_df$max_col, "character")
-  expect_equal(ncol(df) + 1, ncol(arg_max_df))
-})
-
-test_that("picks column with largest value", {
-  arg_max_df <- arg_max_cols(df, num_cols = ncol(df))
-
-  expect_equal(c("a", "c"), arg_max_df$max_col)
-})
-
-test_that("picks column with largest value in restricted range", {
-  arg_max_df <- arg_max_cols(df, num_cols = ncol(df) - 1)
-
-  expect_equal(c("b", "c"), arg_max_df$max_col)
-})
-
-test_that("names correct column", {
-  arg_max_df <- arg_max_cols(df, num_cols = ncol(df), max_col_name = "top_dog")
-
-  expect_equal("top_dog", colnames(arg_max_df)[ncol(arg_max_df)])
-})
-
-
 # %notin% -----------------------------------------------------------------
 
 test_that("%notin% does its job", {
