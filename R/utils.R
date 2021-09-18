c79e0300b53d6834af1b6a6bd21dbc3f2ee6cc46
Bertrand Wilden <bert@bwilden.com>
2021-09-17 21:08:43 -0700

    dont steal my key

--- R/utils.R
+++ R/utils.R
@@ -2,4 +2,5 @@
 # The opposite of %in%
 `%notin%` <- Negate(`%in%`)
 
-Sys.setenv(CENSUS_KEY = "5e4c2b8438222753a7f4753fa78855eca73b9950")
+# Sys.setenv(CENSUS_KEY = "5e4c2b8438222753a7f4753fa78855eca73b9950")
+
