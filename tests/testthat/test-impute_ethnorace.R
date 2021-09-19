4e7abab620747bfaa1baa823506a4c4ceec4d79f
Bertrand Wilden <bert@bwilden.com>
2021-09-19 11:23:33 -0700

    slimmer test data for speed

--- tests/testthat/test-impute_ethnorace.R
+++ tests/testthat/test-impute_ethnorace.R
@@ -1,6 +1,9 @@
 
+test_persons <- example_persons %>%
+  select(first_name, party, state)
+
 imputations_test <-
-  impute_ethnorace(input_data = example_persons,
+  impute_ethnorace(input_data = test_persons,
                    year = 2010,
                    census_key = "5e4c2b8438222753a7f4753fa78855eca73b9950")
 
--- tests/testthat/test-load_bper_data.R
+++ tests/testthat/test-load_bper_data.R
@@ -1,5 +1,8 @@
 
-bper_data_test <- load_bper_data(input_data = example_persons,
+test_persons <- example_persons %>%
+  select(first_name, party, state)
+
+bper_data_test <- load_bper_data(input_data = test_persons,
                                  year = 2010,
                                  census_key = "5e4c2b8438222753a7f4753fa78855eca73b9950")
 
