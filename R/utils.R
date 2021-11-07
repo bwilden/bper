42d225454cca9059d46d5df681fbe923bc8b0ca0
Bertrand Wilden <bert@bwilden.com>
2021-11-06 18:26:42 -0700

    added .prod for less na.rm = TRUE spam

--- R/utils.R
+++ R/utils.R
@@ -2,5 +2,8 @@
 # The opposite of %in%
 `%notin%` <- Negate(`%in%`)
 
+# Product function with na.rm = TRUE as default
+.prod <- purrr::partial(prod, na.rm = TRUE)
+
 # Sys.setenv(CENSUS_KEY = "5e4c2b8438222753a7f4753fa78855eca73b9950")
 
