6b5eab2874392b8248303cd9939da1cde3bfc153
Bertrand Wilden <bert@bwilden.com>
2022-02-28 12:39:27 -0800

    print statements

--- R/load_bper_data.R
+++ R/load_bper_data.R
@@ -28,67 +28,86 @@ load_bper_data <- function(input_data,
 
   input_vars <- c()
   if ("last_name" %in% colnames(input_data)) {
+    print("Loading last names data")
     last_name <- load_surnames_data(year = year)
     input_vars <- c(input_vars, "last")
   }
   if ("first_name" %in% colnames(input_data)) {
+    print("Loading first names data")
     first_name <- load_first_names_data()
     input_vars <- c(input_vars, "first")
   }
   if ("party" %in% colnames(input_data)) {
+    print("Loading political party data")
     party <- load_parties_data(year = year)
     input_vars <- c(input_vars, "party")
   }
   if ("multi_unit" %in% colnames(input_data)) {
+    print("Loading multi-unit data")
     multi_unit <- load_multi_unit_data(year = year)
     input_vars <- c(input_vars, "multi-unit")
   }
-  if ("sex" %in% colnames(input_data)) {
+  if ("sex" %in% colnames(input_data) &
+      "age" %in% colnames(input_data)) {
+    print("Loading sex and age data")
+    sex_age <- load_sex_age_data(year)
+    sex <- sex_ages$sexes
+    age <- sex_ages$ages
+    input_vars <- c(input_vars, "sex", "age")
+  } else if ("sex" %in% colnames(input_data)) {
+    print("Loading sex data")
     sex <- load_sex_age_data(year)$sexes
     input_vars <- c(input_vars, "sex")
-  }
-  if ("age" %in% colnames(input_data)) {
+  } else if ("age" %in% colnames(input_data)) {
+    print("Loading age data")
     age <- load_sex_age_data(year)$ages
     input_vars <- c(input_vars, "age")
   }
 
   if ("state" %in% colnames(input_data)) {
+    print("Loading state data")
     input_states <- unique(input_data$state)
     state <-
       load_geo_data(geo_level = "state",
                     states = input_states,
                     year = year)
     if ("county" %in% colnames(input_data)) {
+      print("Loading county data")
       county <-
         load_geo_data(geo_level = "county",
                       states = input_states,
                       year = year)
     }
     if ("zip" %in% colnames(input_data)) {
+      print("Loading ZIP data")
       zip <-
         load_geo_data(geo_level = "zip",
                       states = input_states,
                       year = year)
     }
     if ("place" %in% colnames(input_data)) {
+      print("Loading place data")
       place <-
         load_geo_data(geo_level = "place",
                       states = input_states,
                       year = year)
     }
     if ("tract" %in% colnames(input_data)) {
+      print("Loading tract data")
       tract <-
         load_geo_data(geo_level = "tract",
                       states = input_states,
                       year = year)
     }
     if ("district" %in% colnames(input_data)) {
+      print("Loading district data")
       district <-
         load_geo_data(geo_level = "congressional district",
                       states = input_states,
                       year = year)
     }
     if ("block" %in% colnames(input_data)) {
+      print("Loading block data")
       block <-
         load_geo_data(geo_level = "block",
                       state = input_states,
